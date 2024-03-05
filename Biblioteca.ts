3ce504ec6dee86310a43f33ee6817dbcb4ee5a10
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-03-05 19:31:52 -0300

    Update Biblioteca.ts

--- Biblioteca.ts
+++ Biblioteca.ts
@@ -23,6 +23,7 @@ export class Biblioteca {
         if (livro.status === StatusLivro.Disponivel) {
             livro.status = StatusLivro.Emprestado;
             usuario.historicoEmprestimos.push(livro);
+            console.log(`O livro:${livro.titulo} foi emprestado para o Usuario:${usuario.nome}`)
             return true;
         }
         return false;
@@ -33,18 +34,20 @@ export class Biblioteca {
         if (index !== -1) {
             livro.status = StatusLivro.Disponivel;
             usuario.historicoEmprestimos.splice(index, 1);
+            console.log(`O livro:${livro.titulo} foi devolvido pelo Usuario:${usuario.nome}`)
         }
     }
 
     reservarLivro(livro: Livro, usuario: Usuario) {
         if (livro.status !== StatusLivro.Disponivel) {
             livro.status = StatusLivro.Reservado;
+            console.log(`O livro:${livro.titulo} foi reservado para o Usuario:${usuario.nome}`)
         }
     }
 
-    verificarMulta(usuario: Usuario): number {
-        let multa = 0;
-        const diasAtraso = this.calcularDiasAtraso(usuario);
+    verificarMulta(usuario: Usuario, multa: number): number {
+        multa;
+        const diasAtraso = this.calcularDiasAtraso(usuario, multa);
         
         if (diasAtraso > 0) {
             multa = diasAtraso * 5; 
@@ -52,9 +55,9 @@ export class Biblioteca {
         return multa;
     }
 
-    calcularDiasAtraso(usuario: Usuario): number {
+    calcularDiasAtraso(usuario: Usuario, diasAtrasoTotal: number): number {
         const prazoDevolucaoPadraoEmDias = 14; 
-        let diasAtrasoTotal = 0; 
+        diasAtrasoTotal
     
         // Percorrendo o histórico de empréstimos do usuário
     for (const livro of usuario.historicoEmprestimos) {
@@ -101,8 +104,8 @@ let livro1 = new Livro("Dom Quixote", "Miguel de Cervantes", StatusLivro.Disponi
 let livro2 = new Livro("1984", "George Orwell", StatusLivro.Emprestado, [GeneroLivro.FiccaoCientifica, GeneroLivro.Suspense]);
 let livro3 = new Livro("O Senhor dos Anéis", "J.R.R. Tolkien", StatusLivro.Atrasado, [GeneroLivro.Fantasia]);
 
-let usuario1 = new Usuario("Franco", 1, "Franco@example.com");
-let usuario2 = new Usuario("Franca", 2, "Franca@exemple.com");
+let usuario1 = new Usuario("Franco", 1, "Franco@example.com", 0);
+let usuario2 = new Usuario("Franca", 2, "Franca@exemple.com", 3);
 
 biblioteca.adicionarLivro(livro1);
 biblioteca.adicionarLivro(livro2);
@@ -115,6 +118,9 @@ console.log("Livros Disponíveis:", biblioteca.listarLivrosDisponiveis());
 console.log("Livros Emprestados:", biblioteca.listarLivrosEmprestados());
 console.log("Livros Atrasados:", biblioteca.listarLivrosAtrasados());
 console.log("Livros de Romance Disponíveis:", biblioteca.listarLivrosPorGenero(GeneroLivro.Romance));
-console.log("Calculando dias de atraso", biblioteca.calcularDiasAtraso(usuario1))
+console.log("Calculando dias de atraso", biblioteca.calcularDiasAtraso(usuario1, 3))
+console.log("Calculando dias de atraso", biblioteca.calcularDiasAtraso(usuario2, 5))
+console.log("Calculando multa de atraso", biblioteca.verificarMulta(usuario1, 3))
 
-
+biblioteca.emprestarLivro(livro1, usuario1)
+biblioteca.devolverLivro(livro1, usuario1)
