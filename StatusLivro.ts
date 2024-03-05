118e27461f13f4f9cad875909542ce485918c460
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-03-04 21:36:39 -0300

    Create StatusLivro.ts

---
+++ StatusLivro.ts
@@ -0,0 +1,7 @@
+// Enum para representar o estado do livro
+ export enum StatusLivro {
+    Disponivel = "Disponivel",
+    Emprestado = "Emprestado",
+    Atrasado = "Atrasado",
+    Reservado = "Reservado"
+}
