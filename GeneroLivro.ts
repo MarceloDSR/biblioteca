91dc47774e414e746ab0455a5db79cf1e8c07a22
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-03-10 15:36:17 -0300

    Update GeneroLivro.ts

--- GeneroLivro.ts
+++ GeneroLivro.ts
@@ -1,11 +1,8 @@
-// Enum para representar os gêneros dos livros
-export enum GeneroLivro {
-    FiccaoCientifica = "Ficcao Cientifica",
-    Romance = "Romance",
-    Fantasia = "Fantasia",
-    Suspense = "Suspense",
-    Terror = "Terror",
-    NaoFiccao = "Nao Ficcao",
-    Poesia = "Poesia",
-    Historia = "Historia"
+// Enum para representar os gêneros literários
+export enum Genero {
+    FiccaoCientifica,
+    Romance,
+    Fantasia,
+    Terror,
+    Suspense
 }
