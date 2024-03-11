1cefa0734ce925bb58a03dffb51267306394bcf7
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-03-11 19:38:02 -0300

    Update Usuarios.ts

--- Usuarios.ts
+++ Usuarios.ts
@@ -1,17 +1,20 @@
 import { StatusUsuario } from "./StatusUsuario";
+import { Livro } from "./Livro";
 
+
+// Definindo a classe Usuario
 export class Usuario {
-    nome: string;
-    id: number;
-    contato: string;
-    historicoEmprestimos: Livro[];
-    status: StatusUsuario; // Adicionando a propriedade status
-
-    constructor(nome: string, id: number, contato: string, status: StatusUsuario) {
-        this.nome = nome;
-        this.id = id;
-        this.contato = contato;
-        this.historicoEmprestimos = [];
-        this.status = status; // Definindo o status ao criar um usuário
+    nome: string; // Propriedade para armazenar o nome do usuário
+    id: number; // Propriedade para armazenar o ID do usuário
+    contato: string; // Propriedade para armazenar as informações de contato do usuário
+    historicoEmprestimos: Livro[]; // Propriedade para armazenar o histórico de empréstimos do usuário
+    
+    // Construtor da classe Usuario
+    constructor(nome: string, id: number, contato: string) {
+        this.nome = nome; // Inicializa a propriedade 'nome' com o valor passado como argumento
+        this.id = id; // Inicializa a propriedade 'id' com o valor passado como argumento
+        this.contato = contato; // Inicializa a propriedade 'contato' com o valor passado como argumento
+        this.historicoEmprestimos = []; // Inicializa a propriedade 'historicoEmprestimos' como uma array vazia
+       
     }
 }
