0609ee0e7b887ba9fae03bd0cb0456c584c76976
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-03-11 19:37:36 -0300

    Update StatusLivro.ts

--- StatusLivro.ts
+++ StatusLivro.ts
@@ -1,7 +1,7 @@
- // Enum para representar o status do livro
- export enum StatusLivro {
-    Disponivel = "Disponivel",
-    Emprestado = "Emprestado",
-    Atrasado = "Atrasado",
-    Reservado = "Reservado"
+// Enumeração para representar o status do livro
+export enum StatusLivro {
+    Disponivel = "Disponivel", // Valor "Disponivel" representa que o livro está disponível para empréstimo
+    Emprestado = "Emprestado", // Valor "Emprestado" representa que o livro está emprestado
+    Atrasado = "Atrasado", // Valor "Atrasado" representa que o livro está em atraso na devolução
+    Reservado = "Reservado" // Valor "Reservado" representa que o livro está reservado
 }
