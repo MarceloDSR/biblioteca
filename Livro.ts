519b9ab3bfb3d5d757ac428067f0ec60230a0093
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-03-11 19:37:24 -0300

    Update Livro.ts

--- Livro.ts
+++ Livro.ts
@@ -1,19 +1,20 @@
 import { StatusLivro } from "./StatusLivro";
 import { Genero } from "./Genero";
 
+// Definindo a classe Livro
 export class Livro {
+    titulo: string; // Propriedade para armazenar o título do livro
+    autor: string; // Propriedade para armazenar o nome do autor do livro
+    generos: Genero[]; // Propriedade para armazenar os gêneros literários do livro
+    status: StatusLivro; // Propriedade para armazenar o status do livro (Disponível, Emprestado, Reservado, etc.)
+    atraso: number; // Propriedade para armazenar o número de dias de atraso na devolução do livro
 
-    titulo: string
-    autor: string
-    generos: Genero[]
-    status: StatusLivro
-    atraso: number
-    
+    // Construtor da classe Livro
     constructor(titulo: string, autor: string, generos: Genero[], status: StatusLivro) {
-        this.titulo = titulo
-        this.autor = autor
-        this.generos = generos
-        this.status = status
-        this.atraso = this.atraso
+        this.titulo = titulo; // Inicializa a propriedade 'titulo' com o valor passado como argumento
+        this.autor = autor; // Inicializa a propriedade 'autor' com o valor passado como argumento
+        this.generos = generos; // Inicializa a propriedade 'generos' com o valor passado como argumento
+        this.status = status; // Inicializa a propriedade 'status' com o valor passado como argumento
+        this.atraso = this.atraso; // Inicializa a propriedade 'atraso' com ela mesma (parece um erro, discutirei abaixo)
     }
 }
